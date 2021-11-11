065cce794a600d644819713b1db570700c3a721e
nashi <nn.contact.n@gmail.com>
2021-11-11 09:52:42 +0900

    change template

--- abc226/c/main.cpp
+++ abc226/c/main.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 using ll = long long int;
 
+template <class T> using V = std::vector<T>;
+
 #define _GLIBCXX_DEBUG
 
 #define rep(i, m, n) for (ll i = (m); (i) < (ll)(n); ++(i))
 
 #define endl '\n'
 #define all(a) (a).begin(), (a).end()
+#define fs first
+#define sc second
+#define pb push_back
+#define ins insert
 
 const ll NUMBER = 1000000007;
 

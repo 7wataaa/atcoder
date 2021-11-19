941cc4b3ec0515648a40d1a8932ccebb4152edbd
nashi <nn.contact.n@gmail.com>
2021-11-19 13:18:28 +0900

    commit

---
+++ joi2021yo1b/c/main.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long int;
+
+template <class T> using V = std::vector<T>;
+
+template <typename T>
+std::ostream &operator<<(std::ostream &stream, const vector<T> v) {
+  for (ll i = 0; i < v.size(); i++) {
+    stream << v[i];
+
+    if (i != v.size() - 1) {
+      stream << ' ';
+    }
+  }
+
+  return stream;
+}
+
+template <typename T, typename V>
+std::ostream &operator<<(std::ostream &stream, const pair<T, V> p) {
+  stream << "(" << p.first << ", " << p.second << ")";
+
+  return stream;
+}
+
+#define _GLIBCXX_DEBUG
+
+#define rep(i, m, n) for (ll i = (m); (i) < (ll)(n); ++(i))
+
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define fs first
+#define sc second
+#define pb push_back
+#define ins insert
+
+const ll NUMBER = 1000000007;
+const int d[5] = {0, 1, 0, -1, 0};
+
+int ctoi(char c) {
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  }
+  return 0;
+}
+
+bool isSameCharAll(string str) {
+  return (str.find_first_not_of(str[0]) == string::npos);
+}
+
+template <class T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+ll vecnum(V<ll> v) {
+  ll result = 0;
+  for (auto i : v) {
+    result += i;
+  }
+  return result;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+
+  V<ll> vec(n);
+
+  ll maxA = -1;
+
+  rep(i, 0, n) {
+    ll a;
+    cin >> a;
+    vec[i] = a;
+    maxA = max(a, maxA);
+  }
+
+  V<ll> v1(vec.begin(), find(all(vec), maxA));
+  V<ll> v2(find(all(vec), maxA) + 1, vec.end());
+
+  cout << vecnum(v1) << endl;
+  cout << vecnum(v2) << endl;
+}

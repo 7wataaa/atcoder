eafaf248c75413016ecf0637644052c124cb31c1
nashi <nn.contact.n@gmail.com>
2022-01-08 20:58:20 +0900

    commit

--- joi2022yo2/d/main.cpp
+++ joi2022yo2/d/main.cpp
@@ -5,7 +5,7 @@ using ll = long long int;
 
 template <class T> using V = std::vector<T>;
 
-template <typename T>
+template <class T>
 std::ostream &operator<<(std::ostream &stream, const vector<T> v) {
   for (ll i = 0; i < v.size(); i++) {
     stream << v[i];
@@ -18,7 +18,7 @@ std::ostream &operator<<(std::ostream &stream, const vector<T> v) {
   return stream;
 }
 
-template <typename T, typename V>
+template <class T, class V>
 std::ostream &operator<<(std::ostream &stream, const pair<T, V> p) {
   stream << "(" << p.first << ", " << p.second << ")";
 
@@ -37,8 +37,11 @@ std::ostream &operator<<(std::ostream &stream, const pair<T, V> p) {
 #define ins insert
 
 const ll NUMBER = 1000000007;
+
+/* 四方向見るための配列 [y + d[i]][x + d[i + 1]]←こんな感じで使う*/
 const int d[5] = {0, 1, 0, -1, 0};
 
+/* char to int */
 int ctoi(char c) {
   if (c >= '0' && c <= '9') {
     return c - '0';
@@ -46,10 +49,12 @@ int ctoi(char c) {
   return 0;
 }
 
+/* すべて同一charで構成された文字列かの判定 */
 bool isSameCharAll(string str) {
   return (str.find_first_not_of(str[0]) == string::npos);
 }
 
+/* DP */
 template <class T> inline bool chmin(T &a, T b) {
   if (a > b) {
     a = b;
@@ -66,20 +71,43 @@ template <class T> inline bool chmax(T &a, T b) {
   return false;
 }
 
-int main() {
-  ll n, m;
-  cin >> n >> m;
+/* Union-find */
+V<ll> par;
 
-  V<ll> T(n);
-  rep(i, 0, n) { cin >> T[i]; }
+ll findRoot(ll x) {
+  if (par[x] == x) {
+    return x;
+  } else {
+    return par[x] = findRoot(par[x]);
+  }
+}
 
-  map<ll, V<ll>> testimonies;
-  rep(i, 0, m) {
-    ll a, b, c;
-    cin >> a >> b >> c;
+bool sameRoot(ll x, ll y) { return findRoot(x) == findRoot(y); }
 
-    testimonies[i] = {a, b, c};
+void uniteRoot(ll x, ll y) {
+  x = findRoot(x);
+  y = findRoot(y);
+
+  if (x == y) {
+    return;
   }
 
+  par[x] = y;
+}
+
+/* 繰り返し二乗法 */
+ll pow_mod(ll n, ll k, ll mod) {
+  ll result = 1;
+  for (; k > 0; k >>= 1) {
+    if (k & 1) {
+      result = (result * n) % mod;
+    }
+    n = (n * n) % mod;
+  }
+  return result;
+}
+
+int main() {
   
 }
+
